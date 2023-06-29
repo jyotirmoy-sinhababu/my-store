52b7b5b9e614997d060c1447bd8389832cd7be94
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-29 23:14:44 +0530

    created a toggle btn

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { BsFillCircleFill } from 'react-icons/bs';
+
+import './homePgStyle.css';
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = () => {
+    if (!isActive) {
+      setIsActive(true);
+    }
+    if (isActive) {
+      setIsActive(false);
+    }
+  };
+
+  return (
+    <button
+      onClick={handleClick}
+      className={`home-btn ${isActive ? 'btnIsOn' : 'btnIsOff'} `}
+    >
+      <BsFillCircleFill />
+    </button>
+  );
 };
 
 export default HomePage;
