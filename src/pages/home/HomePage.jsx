2ca751bc0d988ffbe4f8625579d5fe5f415386d7
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-30 13:09:27 +0530

    changes made

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,29 +1,9 @@
-import React, { useState } from 'react';
-
-import { BsFillCircleFill } from 'react-icons/bs';
+import React from 'react';
 
 import './homePgStyle.css';
 
 const HomePage = () => {
-  const [isActive, setIsActive] = useState(false);
-
-  const handleClick = () => {
-    if (!isActive) {
-      setIsActive(true);
-    }
-    if (isActive) {
-      setIsActive(false);
-    }
-  };
-
-  return (
-    <button
-      onClick={handleClick}
-      className={`home-btn ${isActive ? 'btnIsOn' : 'btnIsOff'} `}
-    >
-      <BsFillCircleFill />
-    </button>
-  );
+  return <div className='main-div'></div>;
 };
 
 export default HomePage;
